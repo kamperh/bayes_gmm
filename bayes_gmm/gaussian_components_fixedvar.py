Added prior class for fixed variance for consistency.

bayes_gmm/gaussian_components_fixedvar.py
@@ -33,13 +33,9 @@ class GaussianComponentsFixedVar(object):
     ----------
     X : NxD matrix
         A matrix of N data vectors, each of dimension D.
-    precision : Dx1 vector
-        The fixed precisions (i.e. the diagonal of the fixed diagonal precision
-        matrix).
-    mu_0 : Dx1 vector
-        The prior mean of the mean of the Gaussian components.
-    var_0 : Dx1 vector
-        The prior variance of the mean of the Gaussian components.
+    prior : `FixedVarPrior`
+        Contains the fixed variance Dx1 vector `var`, the prior mean Dx1 vector
+        `mu_0` and the prior variance Dx1 vector `var_0`.
     assignments : Nx1 vector of int
         The initial component assignments. If this values is None, then all
         data vectors are left unassigned indicated with -1 in the vector.
@@ -76,13 +72,13 @@ class GaussianComponentsFixedVar(object):
         Counts for each of the K components.
     """
 
-    def __init__(self, X, var, mu_0, var_0, assignments=None, K_max=None):
+    def __init__(self, X, prior, assignments=None, K_max=None):
 
         # Attributes from parameters
         self.X = X
-        self.precision = 1./var
-        self.mu_0 = mu_0
-        self.precision_0 = 1./var_0
+        self.precision = 1./prior.var
+        self.mu_0 = prior.mu_0
+        self.precision_0 = 1./prior.var_0
         self.N, self.D = X.shape
         if K_max is None:
             K_max = self.N
@@ -300,6 +296,20 @@ class GaussianComponentsFixedVar(object):
 
 
 #-----------------------------------------------------------------------------#
+#                     FIXED VARIANCE GAUSSIAN PRIOR CLASS                     #
+#-----------------------------------------------------------------------------#
+
+class FixedVarPrior(object):
+    """
+    The prior parameters for a fixed diagonal covariance multivariate Gaussian.
+    """
+    def __init__(self, var, mu_0, var_0):
+        self.var = var
+        self.mu_0 = mu_0
+        self.var_0 = var_0
+
+
+#-----------------------------------------------------------------------------#
 #                              UTILITY FUNCTIONS                              #
 #-----------------------------------------------------------------------------#
 
@@ -324,37 +334,8 @@ def log_post_pred_unvectorized(gmm, i):
 #-----------------------------------------------------------------------------#
 
 def main():
-
-
-
-
-    return
-    # Calculate posterior for first component by hand
-    x_1 = X_1[0]
-    precision_N_1 = precision_0 + N_1*precision
-    mu_N_1 = (mu_0 * precision_0 + precision*N_1*X_1.mean(axis=0)) / precision_N_1
-    precision_pred = 1./(1./precision_N_1 + 1./precision)
-    expected_posterior = np.sum(
-        [log_norm_pdf(x_1[i], mu_N_1[i], 1./precision_pred[i]) for i in range(len(x_1))]
-        )
-    print expected_posterior
-    print gmm.log_post_pred_k(0, 0)
-
-    # Calculate posterior for second component by hand
-    x_3 = X_3[0]
-    precision_N_3 = precision_0 + N_3*precision
-    mu_N_3 = (mu_0 * precision_0 + precision*N_3*X_3.mean(axis=0)) / precision_N_3
-    precision_pred = 1./(1./precision_N_3 + 1./precision)
-    expected_posterior = np.sum(
-        [log_norm_pdf(x_3[i], mu_N_3[i], 1./precision_pred[i]) for i in range(len(x_3))]
-        )
-    print expected_posterior
-    print gmm.log_post_pred_k(N_1 + N_2, 1)
-
-
+    pass
 
 
 if __name__ == "__main__":
     main()
-
-

bayes_gmm/tests/test_gaussian_components_fixedvar.py
@@ -9,7 +9,7 @@ import numpy as np
 import numpy.testing as npt
 
 from bayes_gmm.gaussian_components_fixedvar import (
-    GaussianComponentsFixedVar, log_norm_pdf, log_post_pred_unvectorized
+    GaussianComponentsFixedVar, FixedVarPrior, log_norm_pdf, log_post_pred_unvectorized
     )
 
 
@@ -22,10 +22,11 @@ def test_log_prod_norm():
     var = 1*np.random.rand(D)
     mu_0 = 5*np.random.rand(D) - 2
     var_0 = 2*np.random.rand(D)
+    prior = FixedVarPrior(var, mu_0, var_0)
 
     # GMM will be used to access `_log_prod_norm`
     x = 3*np.random.rand(D) + 4
-    gmm = GaussianComponentsFixedVar(np.array([x]), var, mu_0, var_0)
+    gmm = GaussianComponentsFixedVar(np.array([x]), prior)
 
     expected_prior = np.sum([log_norm_pdf(x[i], mu_0[i], var_0[i]) for i in range(len(x))])
 
@@ -50,12 +51,13 @@ def test_log_post_pred_k():
     var = 1*np.random.rand(D)
     mu_0 = 5*np.random.rand(D) - 2
     var_0 = 2*np.random.rand(D)
+    prior = FixedVarPrior(var, mu_0, var_0)
     precision = 1./var
     precision_0 = 1./var_0
 
     # Setup GMM
     assignments = np.concatenate([np.zeros(N_1), np.ones(N_2), 2*np.ones(N_3)])
-    gmm = GaussianComponentsFixedVar(X, var, mu_0, var_0, assignments=assignments)
+    gmm = GaussianComponentsFixedVar(X, prior, assignments=assignments)
 
     # Remove everything from component 2 (additional check)
     for i in range(N_1, N_1 + N_2):
@@ -96,10 +98,11 @@ def test_log_post_pred():
     var = 1*np.random.rand(D)
     mu_0 = 5*np.random.rand(D) - 2
     var_0 = 2*np.random.rand(D)
+    prior = FixedVarPrior(var, mu_0, var_0)
 
     # Setup GMM
     assignments = [0, 0, 0, 1, 0, 1, 3, 4, 3, 2, -1]
-    gmm = GaussianComponentsFixedVar(X, var, mu_0, var_0, assignments=assignments)
+    gmm = GaussianComponentsFixedVar(X, prior, assignments=assignments)
     expected_log_post_pred = log_post_pred_unvectorized(gmm, 10)
 
     npt.assert_almost_equal(gmm.log_post_pred(10), expected_log_post_pred)
@@ -118,12 +121,13 @@ def test_log_marg_k():
     var = 10*np.random.rand(D)
     mu_0 = 5*np.random.rand(D) - 2
     var_0 = 2*np.random.rand(D)
+    prior = FixedVarPrior(var, mu_0, var_0)
     precision = 1./var
     precision_0 = 1./var_0
 
     # Setup GMM
     assignments = np.concatenate([np.zeros(N_1)])
-    gmm = GaussianComponentsFixedVar(X_1, var, mu_0, var_0, assignments=assignments)
+    gmm = GaussianComponentsFixedVar(X_1, prior, assignments=assignments)
 
     # Calculate marginal for component by hand
     expected_log_marg = np.sum(np.log([